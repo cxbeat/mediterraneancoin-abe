a4571212d5a6dd657d7c722515452c515993dc5b
CXbeat <cxbeat@users.noreply.github.com>
2019-01-28 23:51:54 +0800

    changed to a useable version

--- Abe/DataStore.py
+++ Abe/DataStore.py
@@ -72,8 +72,8 @@ CHAIN_CONFIG = [
     # "code3":"SC0", "address_version":"\x6f", "magic":"\xca\xfe\xba\xbe"},
     #{"chain":"Worldcoin",
     # "code3":"WDC", "address_version":"\x49", "magic":"\xfb\xc0\xb6\xdb"},
-    #{"chain":"Mediterraneancoin",
-    # "code3":"MED", "address_version":"\x33", "magic":"\xfb\xc0\xb6\xdb"},     
+    {"chain":"Mediterraneancoin",
+     "code3":"MED", "address_version":"\x33", "magic":"\xfb\xc0\xb6\xdb"},     
     #{"chain":"",
     # "code3":"", "address_version":"\x", "magic":""},
     ]
